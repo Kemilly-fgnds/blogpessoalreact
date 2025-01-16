8792ee606af81792e92cdec15904db8356825440
Kemilly Fagundes <kemilly.fagundes@outlook.com>
2025-01-16 13:15:35 -0300

    Inserção das páginas de Login e Cadastro

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
-import { Butterfly } from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-purple-700 text-purple-950'>
+            			   bg-pink-300 text-amber-900'>
 
-                <Butterfly size={30} weight='bold' />
                 <div className="container flex justify-between text-lg px-2 font-bold">
-                    Blog Pessoal
+                    <Link to="/home">Blog Pessoal</Link>
 
                     <div className='flex gap-4 px-4 font-bold'>
                         Postagens
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,31 @@
 import { Chats } from '@phosphor-icons/react'
 
-
 function Home() {
     return (
         <>
-            <div className="bg-purple-300 flex justify-center">
-                <div className='container grid grid-cols-2 text-purple-900'>
+            <div className="bg-gray-200 flex justify-center">
+                <div className='container grid grid-cols-2 text-amber-800'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
                             Olá, Seja Bem Vindo!
                         </h2>
-                        <p className='text-xl'>
+                        <p className='text-xl font-semibold text-amber-800'>
                             Expresse aqui seus pensamentos e opiniões.
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='flex items-center gap-2 rounded text-purple-900 
-                                            border-purple-900 border-solid border-2 py-2 px-4 hover:bg-purple-500 font-bold'
+                            <div className='flex items-center gap-2 rounded text-amber-700 
+                                            border-amber-600 border-solid border-2 py-2 px-4 hover:bg-pink-200 font-bold'
                             >
                                 <Chats size={25} weight='bold' />
-                               Nova Postagem
+                                Nova Postagem
                             </div>
                         </div>
                     </div>
 
                     <div className="flex justify-center ">
                         <img
-                            src="https://ik.imagekit.io/dpprdb2sk/foto%20menina.PNG?updatedAt=1736972684071"
+                            src="https://i.imgur.com/loX6KKs.png"
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
