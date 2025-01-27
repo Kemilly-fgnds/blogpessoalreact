bc135a752d1381f8cd897f0a5d1e8a4bc6cf105b
Kemilly Fagundes <kemilly.fagundes@outlook.com>
2025-01-27 09:29:11 -0300

    Deploy do Projeto Integrador

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: 'https://blogpessoal-0zm7.onrender.com/' //roda aplicação local ou remota no render//
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL //roda aplicação local ou remota no render//
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {//async é metodo assíncrono, ele envia a requisição e espera pelo retorno//
